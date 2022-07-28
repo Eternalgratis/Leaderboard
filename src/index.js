7f02b431189883eb0d59957d3af874726c0d925b
Eternalgratis <tyaps002@gmail.com>
2022-07-28 21:13:34 +0100

    Fetch API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-// import _ from 'lodash';
 import './index.css';
+import { fetchScores, addToScore } from './modules/add.js';
+
+const name = document.querySelector('#name');
+const score = document.querySelector('#score');
+const refresh = document.querySelector('#refresh-btn');
+const form = document.querySelector('.input-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToScore(name.value, score.value)
+    form.reset();
+});
+
+refresh.addEventListener('click', () => {
+    fetchScores()
+});
